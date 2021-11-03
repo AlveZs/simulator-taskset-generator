9a895e1388ff5c8a7006ff19e104429f05badcd2
João Alves <alvezsjv@gmail.com>
2021-11-03 19:49:11 -0300

    feat(enviroment): adiciona novas variaveis

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from generator import Generator
 from enviroment import Enviroment
 import sys
 
-utilization = .4
+utilization = round(float(Enviroment.INITAL_UTILIZATION), 1)
 processorsNumber = int(Enviroment.PROCESSORS_NUMBER)
 generationNumber = int(Enviroment.GENERATION_INITIAL_NUMBER)
 decimalPlaces = int(Enviroment.DECIMAL_PLACES)
+step = round(float(Enviroment.STEPS), 2)
 
 print("Quantidade de processadores: ", processorsNumber)
 
-try:
-    while utilization <= 1:
-        for i in range(Enviroment.ROUNDS):
-            Generator(utilization, processorsNumber, generationNumber, decimalPlaces).generate()
-        utilization = round(utilization + .1, 1)
+while utilization <= 1:
+    for i in range(Enviroment.ROUNDS):
+        Generator(utilization, processorsNumber, generationNumber, decimalPlaces).generate()
         generationNumber = generationNumber + 1
-    print("Tarefas geradas com sucesso")
-except:
-    print("Ocorreu um erro ao gerar as tarefas")
+    utilization = round(utilization + step, 2)
+print("Tarefas geradas com sucesso")
