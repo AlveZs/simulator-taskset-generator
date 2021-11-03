61a53ee6206f6d8bc2f294bb7116cf4fee2bd78c
João Alves <alvezsjv@gmail.com>
2021-11-03 15:54:55 -0300

    feat(main): adiciona possobilidade de gerar mais de um conjunto de uma vez

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from generator import Generator
+from enviroment import Enviroment
 import sys
 
 utilization = .4
-generationNumber = 1
-processorsNumber = int(input("Digite o numero de processadores:\n"))
+processorsNumber = int(Enviroment.PROCESSORS_NUMBER)
+generationNumber = int(Enviroment.GENERATION_INITIAL_NUMBER)
+decimalPlaces = int(Enviroment.DECIMAL_PLACES)
+
+print("Quantidade de processadores: ", processorsNumber)
 
 try:
     while utilization <= 1:
-        Generator(utilization, processorsNumber, generationNumber).generate()
+        for i in range(Enviroment.ROUNDS):
+            Generator(utilization, processorsNumber, generationNumber, decimalPlaces).generate()
         utilization = round(utilization + .1, 1)
         generationNumber = generationNumber + 1
     print("Tarefas geradas com sucesso")
