0f1dcea64e7af8795e4439e1ad6084cc3ac27976
João Alves <alvezsjv@gmail.com>
2022-01-07 17:59:12 -0300

    feat(env): adiciona FINAL_UTILIZATION

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from generator import Generator
 from enviroment import Enviroment
 import sys
 
-utilization = round(float(Enviroment.INITAL_UTILIZATION), 1)
+utilization = round(float(Enviroment.INITAL_UTILIZATION), 2)
+finalUtilization = round(float(Enviroment.FINAL_UTILIZATION), 2)
 processorsNumber = int(Enviroment.PROCESSORS_NUMBER)
 generationNumber = int(Enviroment.GENERATION_INITIAL_NUMBER)
 decimalPlaces = int(Enviroment.DECIMAL_PLACES)
@@ -10,7 +11,7 @@ step = round(float(Enviroment.STEP), 2)
 
 print("Quantidade de processadores: ", processorsNumber)
 
-while utilization <= 1:
+while utilization <= finalUtilization:
     for i in range(Enviroment.ROUNDS):
         Generator(utilization, processorsNumber, generationNumber, decimalPlaces).generate()
         generationNumber = generationNumber + 1
